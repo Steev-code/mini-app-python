d992792aa4c18151ad003aac484f758b0b641bd4
Juan David <juanq9939@gmail.com>
2026-03-07 18:39:39 -0500

    conexion render postgres

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ from flask_cors import CORS
 app = Flask(__name__, static_folder="static")
 CORS(app)
 
+# conexión a la base de datos (Render PostgreSQL)
 conexion = psycopg2.connect(
-    host="localhost",
-    database="prueba_web",
-    user="postgres",
-    password="3112230437"
+"postgresql://prueba_web_user:GZ93s12cp2s6TmdKAWDwkgIjf8DszFvW@dpg-d6mbd2fafjfc7390hgk0-a.oregon-postgres.render.com/prueba_web"
 )
 
+# ruta principal
 @app.route("/")
 def inicio():
     return send_from_directory("static", "index.html")
 
 
+# obtener usuarios
 @app.route("/usuarios", methods=["GET"])
 def obtener_usuarios():
 
@@ -34,9 +34,12 @@ def obtener_usuarios():
             "nombre": u[1]
         })
 
+    cursor.close()
+
     return jsonify(lista)
 
 
+# crear usuario
 @app.route("/usuarios", methods=["POST"])
 def crear_usuario():
 
@@ -52,6 +55,8 @@ def crear_usuario():
     )
 
     conexion.commit()
+
+    cursor.close()
 
     return jsonify({"mensaje": "usuario creado"})
 
